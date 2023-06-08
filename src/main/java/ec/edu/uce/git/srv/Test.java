31864ed65c61cd4220d69c71cfe765ee3b257f33
David <davidelpatron22@gmail.com>
2023-06-08 02:43:16 -0500

    clase persona con setters getters

--- src/main/java/ec/edu/uce/git/srv/Test.java
+++ src/main/java/ec/edu/uce/git/srv/Test.java
@@ -1,9 +1,14 @@
 package ec.edu.uce.git.srv;
 
 public class Test {
-
+    persona p1 = new persona("David", 25);
     public void saludo() {
+
         System.out.println("Hola Mundo, mi primer git clone");
         System.out.println("Nuevo aporte, git Actualizado");
+        System.out.println("la edad de"+p1.getNombre()+" es "+p1.getEdad());
     }
+
+
+
 }
